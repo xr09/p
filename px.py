c64ff22f2e92576b629accacdadcd92861f12e82
Manuel Gutierrez <dhunterkde@gmail.com>
2014-11-23 15:47:09 -0500

    Refactoring
    cleaning code

--- px.py
+++ px.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) <2013> <Manuel E. Gutierrez xr09>
+# Copyright (C) 2014 Manuel E. Gutierrez <mgp@nauta.cu>
 #
 #
 # Permission is hereby granted, free of charge, to any person obtaining
@@ -24,46 +24,78 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-
 import sys
 import os
+import re
 
-SERVICES = ['lighttpd', 'memcached', 'postgresql', 'mysql',
-            'prosody', 'ssh', 'wicd', 'hostapd', 'udhcpd', 'dnsmasq']
 
-def main():
-    if len(sys.argv) >= 2:
-        service_to_call = []
-        action_to_do = None
-        for service in SERVICES:
-            if service.startswith(sys.argv[1]):
-                service_to_call.append(service)
-        if len(sys.argv) == 3:
-            action_to_do = sys.argv[2]
-        if not service_to_call:
-            print('no matches')
-        elif len(service_to_call) == 1:
-            print(service_to_call[0])
-            #~ print(action_to_do)
-            if action_to_do in ['0', 'd']:
-                action = 'stop'
-            elif action_to_do in ['1', 'u']:
-                action = 'start'
-            elif action_to_do in ['fr', 'rl']:
-                action = 'force-reload'
-            else:
-                action = 'restart'
-            print(action)
-            os.system("sudo service %s %s" % (service_to_call[0], action))
-            return
+VERSION = '0.2'
+
+# Default service  list
+# Just in case there is no /etc/p.conf
+DEFAULT_SERVICES = "memcached ssh postgresql mysql prosody wicd hostapd \
+            udhcpd dnsmasq apache2 httpd nginx networking php5-fpm uwsgi gunicorn preload \
+            ferm libvirtd".split()
+
+# read service groups form .p.conf
+SERVICE_GROUPS = {'web': 'nginx php5-fpm', 'db': 'postgresql memcached'}
+
+
+def detect_service(name_to_match):
+    """ Match input string against system services.
+    Pick the best match.
+
+    @param name_to_match: input string
+    @return service: detected service name
+    """
+    pattern = re.compile('.*{0}.*'.format(name_to_match), re.IGNORECASE)
+    #~ matched = []
+    #~ for service in DEFAULT_SERVICES:
+        #~ if pattern.match(service):
+            #~ matched.append(service)
+    return [ service for service in DEFAULT_SERVICES if pattern.match(service)]
+    #~ if len(matched) == 1:
+        #~ return matched[0]
+    #~ else:
+        #~ return None
+
+
+def print_help():
+    """ Print basic usage"""
+    USAGE = """P (as in Pragmatic)
+
+version: {0}
+
+P is a simple frontend for managing init systems. No more dealing with 'service', 'systemctl' or '/etc/init.d/', just use p and take advantage of its regex support.
+
+Starting a service: (p regex 1)
+    p mysq 1      # start mysql
+
+Stoping a service: (p regex 0)
+    p pos 0      # stop postgres
+
+Restarting a service: (p regex)
+    p libvi        # restart libvirtd
+
+Remember you must type enough characters to avoid name collisions.
+
+The regex match in the code is like this: .*regex.*, so you can type
+something like 'p emcach 1' and it still matchs 'memcached'.
+""".format(VERSION)
+    print(USAGE)
+
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2:
+        service = detect_service(args[1])
+        count = len(service)
+        if count == 0:
+            print("No service was matched with '%s'" % args[1])
+        elif count == 1:
+            print("do stuff to %s" % service)
         else:
-            print('to many matches')
-            for i,s in enumerate(service_to_call):
-                print "%i -> %s" % (i+1, s)
-        return 0
-    print 'wrong args'
-    return 1
-
-if __name__ == '__main__':
-    main()
-
+            print("to many matchs: %s" % service)
+    else:
+        print_help()
